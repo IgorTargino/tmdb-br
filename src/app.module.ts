4f9f41493549ffef3ed838cc17c6d716846a18ac
IgorTargino <igortargino01@gmail.com>
2023-07-12 01:24:30 -0300

    docs: add swagger and configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpModule } from './infra/http/http.module';
+import { ConfigModule } from '@nestjs/config';
 
+@Global()
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('MM API')
+    .setDescription('My Movies API')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .addServer('http://localhost:3000', 'Local server')
+    .addServer(
+      'https://filmes-famosos-br-api-heroku.herokuapp.com',
+      'Heroku server',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
