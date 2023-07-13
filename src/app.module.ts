abe174f0f256c31a3e94543b5acfb0ab20e9d0ae
IgorTargino <igortargino01@gmail.com>
2023-07-13 08:49:46 -0300

    chore: remove app controller/service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Global, Logger, Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HttpModule } from './infra/http/http.module';
 import { ConfigModule } from '@nestjs/config';
 import { ConstantKeys } from './infra/secrets/enums/constant-keys.enum';
 import { SecretProvider } from './infra/secrets/secret.provider';
 import { SecretModule } from './infra/secrets/secret.module';
+import { DatabaseModule } from './infra/database/database.module';
 
 const secretProvider = Object.values(ConstantKeys).map((key) => ({
   provide: key,
@@ -17,9 +16,14 @@ const secretProvider = Object.values(ConstantKeys).map((key) => ({
 }));
 @Global()
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), SecretModule, HttpModule],
-  controllers: [AppController],
-  providers: [AppService, Logger, ...secretProvider],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SecretModule,
+    HttpModule,
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [Logger, ...secretProvider],
   exports: [...secretProvider],
 })
 export class AppModule {}
