2afb4c311a746d8e96787b3580405b4c1ebb3f0b
IgorTargino <igortargino01@gmail.com>
2023-07-13 03:13:18 -0300

    doc: add apitags to movie controller

--- src/infra/http/controllers/movie.controller.ts
+++ src/infra/http/controllers/movie.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { GetMostPopularMoviesBrService } from 'src/domain/use-cases/movies/get-most-popular-movies-br.service';
 import { AuthGuard } from '../auth/auth.guard';
+import { ApiTags } from '@nestjs/swagger';
 
 @UseGuards(AuthGuard)
 @Controller('movie')
+@ApiTags('movie')
 export class MovieController {
   constructor(
     private readonly getMostPopularMoviesBrService: GetMostPopularMoviesBrService,
